087566cf6aa0c0ac7d30703bd3b4fe4c5531a7e4
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-03 19:10:24 +0000

    Setup Homescreen and nav component

---
+++ src/HomeScreen.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./HomeScreen.scss"
+import Nav from './Nav'
+
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            <Nav />
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+        </div>
+    )
+}
+
+export default HomeScreen
