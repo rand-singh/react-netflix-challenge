ea83bb342103cac703bcccefbf1962aedf75178a
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-03 18:34:52 +0000

    Create React App

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzbbsRBkuk1MOGHU1OBdvrw__jWWbkLjg",
+    authDomain: "react-netflix-challenge.firebaseapp.com",
+    projectId: "react-netflix-challenge",
+    storageBucket: "react-netflix-challenge.appspot.com",
+    messagingSenderId: "698993273939",
+    appId: "1:698993273939:web:4d66acb7deaa03d7446aa6"
+  };
