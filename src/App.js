e65f19500fef8e90b6f92319a4b37026ea9aeee9
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-04 20:09:59 +0000

    Use redux state in app.js comp for auth

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React, { useEffect } from 'react';
 import './App.scss';
 import HomeScreen from './screens/HomeScreen'
 import LoginScreen from './screens/LoginScreen'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice'
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
         console.log('userAuth',userAuth)
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email
+          })
+        )
       } else {
+        dispatch(logout)
       }
     })
     return unsubscribe
