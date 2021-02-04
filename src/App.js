1e326b0a29f1ed67aeb6e1fd4ac03b86c06a6cda
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-04 18:29:50 +0000

    Install react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.scss';
 import HomeScreen from './HomeScreen'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
