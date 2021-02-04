649e8f3932a3d735ffa080556d55e9fdde77204d
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-04 20:01:42 +0000

    Install firebase, setup google auth, setup redux to handle user auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import HomeScreen from './screens/HomeScreen'
 import LoginScreen from './screens/LoginScreen'
@@ -7,9 +7,20 @@ import {
   Switch,
   Route
 } from 'react-router-dom'
+import { auth } from './firebase';
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        console.log('userAuth',userAuth)
+      } else {
+      }
+    })
+    return unsubscribe
+  }, [])
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyCzbbsRBkuk1MOGHU1OBdvrw__jWWbkLjg",
     authDomain: "react-netflix-challenge.firebaseapp.com",
@@ -5,4 +7,11 @@ const firebaseConfig = {
     storageBucket: "react-netflix-challenge.appspot.com",
     messagingSenderId: "698993273939",
     appId: "1:698993273939:web:4d66acb7deaa03d7446aa6"
-  };
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { auth }
+export default db
